4afb96db12c1dff1089965c95da922da7873dc23
LuuHongDongMy <mylhdse160913@fpt.edu.vn>
2022-07-30 12:34:17 +0700

    myluu

--- src/wrappers/product/ProfileDescriptionTab.js
+++ src/wrappers/product/ProfileDescriptionTab.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useEffect,useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Tab from "react-bootstrap/Tab";
 import Nav from "react-bootstrap/Nav";
 import ShopProducts from "./ShopProducts";
@@ -17,7 +17,7 @@ const ProfileDescriptionTab = ({ spaceBottomClass, productFullDesc, posts, layou
   // const [currentPage2, setCurrentPage2] = useState(1);
   // const [currentData2, setCurrentData2] = useState([]);
 
-  const imgRef=useRef();
+  const imgRef = useRef();
 
   useEffect(() => {
     // setCurrentData(posts.slice(offset, offset + pageLimit));
@@ -25,15 +25,15 @@ const ProfileDescriptionTab = ({ spaceBottomClass, productFullDesc, posts, layou
 
     const img = imgRef.current;
 
-    const observer = new IntersectionObserver(entries=>{
+    const observer = new IntersectionObserver(entries => {
       console.log(entries[0].isIntersecting)
     })
 
     //kich hoat observer len
-    if(img) observer.observe(img)
+    if (img) observer.observe(img)
 
-    return()=>{
-      if(img) observer.unobserve(img)
+    return () => {
+      if (img) observer.unobserve(img)
 
     }
   }, [])
@@ -51,9 +51,7 @@ const ProfileDescriptionTab = ({ spaceBottomClass, productFullDesc, posts, layou
               <Nav.Item>
                 <Nav.Link eventKey="productDescription">Sold</Nav.Link>
               </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="productReviews">Reviews</Nav.Link>
-              </Nav.Item>
+
             </Nav>
             <Tab.Content className="description-review-bottom">
               <Tab.Pane eventKey="additionalInfo">
@@ -88,7 +86,7 @@ const ProfileDescriptionTab = ({ spaceBottomClass, productFullDesc, posts, layou
                 </div> */}
                 {posts && posts.map((product) => <div>
                   <Link to={process.env.PUBLIC_URL + "/product/" + product.id}>
-                  {product.images !== null ? (
+                    {product.images !== null ? (
                       <img
                         className="default-img img-fluid"
                         src={product.images[0].image}
@@ -124,13 +122,13 @@ const ProfileDescriptionTab = ({ spaceBottomClass, productFullDesc, posts, layou
                       <div>ok</div>
 
                     )} */}
-                    
+
                   </Link>
                   {product.name}
                 </div>
-                              )}
+                )}
 
-                
+
               </Tab.Pane>
               <Tab.Pane eventKey="productDescription">
                 {/* <ShopProducts layout={layout} products={p} />
@@ -149,138 +147,20 @@ const ProfileDescriptionTab = ({ spaceBottomClass, productFullDesc, posts, layou
                                 </div> */}
                 alos2
               </Tab.Pane>
-              <Tab.Pane eventKey="productReviews">
-                <div className="row">
-                  <div className="col-lg-7">
-                    <div className="review-wrapper">
-                      <div className="single-review">
-                        <div className="review-img">
-                          <img
-                            src={
-                              process.env.PUBLIC_URL +
-                              "/assets/img/testimonial/1.jpg"
-                            }
-                            alt=""
-                          />
-                        </div>
-                        <div className="review-content">
-                          <div className="review-top-wrap">
-                            <div className="review-left">
-                              <div className="review-name">
-                                <h4>White Lewis</h4>
-                              </div>
-                              <div className="review-rating">
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                              </div>
-                            </div>
-                            <div className="review-left">
-                              <button>Reply</button>
-                            </div>
-                          </div>
-                          <div className="review-bottom">
-                            <p>
-                              Vestibulum ante ipsum primis aucibus orci
-                              luctustrices posuere cubilia Curae Suspendisse
-                              viverra ed viverra. Mauris ullarper euismod
-                              vehicula. Phasellus quam nisi, congue id nulla.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="single-review child-review">
-                        <div className="review-img">
-                          <img
-                            src={
-                              process.env.PUBLIC_URL +
-                              "/assets/img/testimonial/2.jpg"
-                            }
-                            alt=""
-                          />
-                        </div>
-                        <div className="review-content">
-                          <div className="review-top-wrap">
-                            <div className="review-left">
-                              <div className="review-name">
-                                <h4>White Lewis</h4>
-                              </div>
-                              <div className="review-rating">
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                                <i className="fa fa-star" />
-                              </div>
-                            </div>
-                            <div className="review-left">
-                              <button>Reply</button>
-                            </div>
-                          </div>
-                          <div className="review-bottom">
-                            <p>
-                              Vestibulum ante ipsum primis aucibus orci
-                              luctustrices posuere cubilia Curae Suspendisse
-                              viverra ed viverra. Mauris ullarper euismod
-                              vehicula. Phasellus quam nisi, congue id nulla.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-lg-5">
-                    <div className="ratting-form-wrapper pl-50">
-                      <h3>Add a Review</h3>
-                      <div className="ratting-form">
-                        <form action="#">
-                          <div className="star-box">
-                            <span>Your rating:</span>
-                            <div className="ratting-star">
-                              <i className="fa fa-star" />
-                              <i className="fa fa-star" />
-                              <i className="fa fa-star" />
-                              <i className="fa fa-star" />
-                              <i className="fa fa-star" />
-                            </div>
-                          </div>
-                          <div className="row">
-                            <div className="col-md-6">
-                              <div className="rating-form-style mb-10">
-                                <input placeholder="Name" type="text" />
-                              </div>
-                            </div>
-                            <div className="col-md-6">
-                              <div className="rating-form-style mb-10">
-                                <input placeholder="Email" type="email" />
-                              </div>
-                            </div>
-                            <div className="col-md-12">
-                              <div className="rating-form-style form-submit">
-                                <textarea
-                                  name="Your Review"
-                                  placeholder="Message"
-                                  defaultValue={""}
-                                />
-                                <input type="submit" defaultValue="Submit" />
-                              </div>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            </div>
+
+
+
+            </Tab.Content>
+          </Tab.Container>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 ProfileDescriptionTab.propTypes = {
-    productFullDesc: PropTypes.string,
-    spaceBottomClass: PropTypes.string,
+  productFullDesc: PropTypes.string,
+  spaceBottomClass: PropTypes.string,
 };
 
 export default ProfileDescriptionTab;
