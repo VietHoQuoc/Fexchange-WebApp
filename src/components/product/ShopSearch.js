bd95628f8028816522ae3f8360c41f0fa4d89d82
hdangnguyen <haidangprod@gmail.com>
2022-08-03 02:45:40 +0700

    fix search auto reload

--- src/components/product/ShopSearch.js
+++ src/components/product/ShopSearch.js
@@ -1,20 +1,24 @@
-import React from "react";
-import { useState } from "react";
-const ShopSearch = ({getSearchParams}) => {
-  const [keyword, setKeyword]=useState('');
-  return (
-    <div className="sidebar-widget">
-      <h4 className="pro-sidebar-title">Search </h4>
-      <div className="pro-sidebar-search mb-50 mt-25">
-        <form className="pro-sidebar-search-form">
-          <input type="text" placeholder="Search here..." onChange={event=>setKeyword(event.target.value)} />
-          <button  onClick={()=>getSearchParams('search',keyword)}>
-            <i className="pe-7s-search" />
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+import React from 'react';
+import { useState } from 'react';
+const ShopSearch = ({ getSearchParams }) => {
+    const [keyword, setKeyword] = useState('');
+    return (
+        <div className="sidebar-widget">
+            <h4 className="pro-sidebar-title">Search </h4>
+            <div className="pro-sidebar-search mb-50 mt-25">
+                <div className="pro-sidebar-search-form">
+                    <input
+                        type="text"
+                        placeholder="Search here..."
+                        onChange={(event) => setKeyword(event.target.value)}
+                    />
+                    <button onClick={() => getSearchParams('search', keyword)}>
+                        <i className="pe-7s-search" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ShopSearch;
